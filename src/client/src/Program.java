1bf111a97191361bd8084c9e1274fb2bf22315a9
Richard Matuch <matuchrichard@citromail.hu>
2022-05-03 19:40:20 +0200

    Func.: Reading configuration file
    Added loadConfiguration function to server and client.

--- src/client/src/Program.java
+++ src/client/src/Program.java
@@ -1,5 +1,35 @@
-public class Program {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Program {
+    private static String port;
+    private static String ip;
+    public static void main(String[] args) {
+        loadConfiguration();
+        System.out.println(port);
+        System.out.println(ip);
+    }
+    private static boolean loadConfiguration()
+    {
+        try {
+            FileInputStream fis = new FileInputStream("app.config");
+            Properties prop = new Properties();
+            prop.load(fis);
+            port = prop.getProperty("port");
+            ip = prop.getProperty("ip");
+            fis.close();
+            return true;
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("File app.config could not be found!");
+            ex.printStackTrace();
+        }
+        catch (IOException ex) {
+            System.out.println("Unexpected IO error!");
+            ex.printStackTrace();
+        }
+        return false;
     }
 }
